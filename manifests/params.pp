3991906a83997b7b42f44fd9f651a2862ac9726a
Saz <me@saz.sh>
2011-09-14 15:16:55 +0200

    remove some parameters which are already set through init script

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,11 +12,6 @@ class gearman::params {
         default: { $job_retries = "--job-retries=${gearman_job_retries}" }
     }
 
-    case $gearman_log_file {
-        '': { $log_file = '--log-file=/var/log/gearman-job-server/gearman.log' }
-        default: { $log_file = "--log-file=${gearman_log_file}" }
-    }
-
     case $gearman_port {
         '': { $port = '--port=4730' }
         default: { $port = "--port=${gearman_port}" }
@@ -30,10 +25,5 @@ class gearman::params {
     case $gearman_threads {
         '': { $threads = '--threads=4' }
         default: { $threads = "--threads=${gearman_threads}" }
-    }
-
-    case $gearman_user {
-        '': { $user = '--user=gearman' }
-        default: { $user = "--user=${gearman_user}" }
     }
 }
